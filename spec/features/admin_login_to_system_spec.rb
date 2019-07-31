4c1185d70c0a797b98711250062abc440a0ddd01
Henrique Morato <riquemorato@gmail.com>
2019-07-31 18:20:01 -0300

    Login tests

---
+++ spec/features/admin_login_to_system_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature 'Admin login to system' do
+  scenario 'successfully' do
+    user = create(:user)
+
+    visit root_path
+    fill_in 'Email', with: user.email
+    fill_in 'Senha', with: user.password
+    click_on 'Log in'
+
+    expect(page).to have_content(user.email)
+    expect(page).to have_link('Logout')
+  end
+
+  scenario 'and log out' do
+    user = create(:user)
+
+    visit root_path
+    fill_in 'Email', with: user.email
+    fill_in 'Senha', with: user.password
+    click_on 'Log in'
+    click_on 'Logout'
+
+    expect(page).not_to have_content(user.email)
+    expect(page).to have_link('Login')
+  end
+end
