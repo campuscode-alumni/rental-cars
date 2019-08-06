823bbc94209e3808d5f43e827508be1ba2212037
Rafael e Renan <treinamentos+par02@campuscode.com.br>
2019-08-05 22:12:28 -0300

    Cria teste de retorno de manutencao

---
+++ spec/features/user_return_car_from_maintenance_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+feature 'User return car from maintenance' do
+  scenario 'successfully' do
+    # dados
+    user = create(:user)
+    provider = Provider.create(name: 'Solucoes.ltda', cnpj: '1234567/777')
+    fiat = create(:manufacture, name: 'Fiat')
+    palio = create(:car_model, name: 'Palio', manufacture: fiat )
+    car = create(:car, car_model: palio, license_plate: "xlg1234" )
+    maintenance = create(:maintenance, car: car, provider: provider)
+
+    # acoes
+    login_as user, scope: :user
+    visit root_path
+    
+    click_on 'Carros em Manutenção'
+    click_on 'Palio-xlg1234'
+    fill_in 'Nota Fiscal', with: 'NF0137'
+    fill_in 'Valor', with: '2500'
+    click_on 'Retornar de manutenção'
+
+    # expectativas
+    expect(page).to have_content('NF0137')
+    expect(page).to have_content('2500')
+    expect(page).to have_content('Carro retornou da manutenção')
+    expect(current_path).to eq maintenance_path(maintenance)
+    expect(page).to have_content('Status: Disponível')
+    expect(page).not_to have_link('Retornar de manutenção')
+  end
+end
