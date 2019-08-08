cda0028f4ce4b656081ab8f82b55e9d3959b83d5
Henrique Morato <riquemorato@gmail.com>
2019-08-08 19:07:07 -0300

    I18n to cars

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,15 +2,10 @@ class Car < ApplicationRecord
   belongs_to :car_model
   belongs_to :subsidiary
   has_many :maintenances
-  
+
   enum status: { available: 0, on_maintenance: 5 }
 
-  validates :car_model, presence: { message: 'Modelo não pode ficar em branco' }
-  validates :car_km, presence: { message: 'Quilometragem não pode ficar '\
-                                          'em branco' }
-  validates :color, presence: { message: 'Cor não pode ficar em branco' }
-  validates :license_plate, presence: { message: 'Placa não pode '\
-                                                 'ficar em branco' }
+  validates :car_model, :car_km, :color, :license_plate, presence: true
 
   def car_identification
     "#{car_model.name} - #{license_plate}"
