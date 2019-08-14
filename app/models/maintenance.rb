4dd650bfe4c3f396314a064815d8b08be0b648f1
joao e mateus <treinamentos+par01@campuscode.com.br>
2019-08-13 21:16:52 -0300

    adicionado i18n e ajustado o metodo car return

--- app/models/maintenance.rb
+++ app/models/maintenance.rb
@@ -2,19 +2,23 @@ class Maintenance < ApplicationRecord
   belongs_to :car
   belongs_to :provider
   has_one    :debit, as: :transactable
+  
+  validates :invoice, 
+            :service_cost, 
+            presence: true, 
+            on: :update
 
   scope :cars_on_maintenance, -> do
     joins(:car).where(cars: { status: :on_maintenance })
   end
 
   def car_return(params)
-    errors.add(:invoice, 'Nota Fiscal não pode ficar em branco') if params[:invoice].blank?
-    errors.add(:service_cost, 'Valor não pode ficar em branco') if params[:service_cost].blank?
-    return false if errors.any?
-
-    update(params)
-    car.available!
-    create_debit(amount: params[:service_cost],
-                 subsidiary: car.subsidiary)
+    if update(params)
+      car.available!
+      create_debit(amount: params[:service_cost],
+                   subsidiary: car.subsidiary)
+    else
+      return false
+    end
   end
 end
