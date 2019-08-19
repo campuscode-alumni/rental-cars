34d6703e1902d7d1cdc8a1737090af5f866d4db3
Lucas e Rafael <treinamentos+par03@campuscode.com.br>
2019-08-19 19:20:18 -0300

    Adiciona a tratativa do erro para o ApiController

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,3 +1,5 @@
 class Api::V1::ApiController < ActionController::API
-  
+  rescue_from ActiveRecord::RecordNotFound do
+    render json: "Not found", status: 404
+  end
 end
--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -3,7 +3,5 @@ class Api::V1::CarsController < Api::V1::ApiController
     car = Car.find(params[:id])
 
     render json: car, status: 200
-  rescue ActiveRecord::RecordNotFound
-    render json: "Not found", status: 404
   end
 end
